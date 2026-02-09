33b15bce40f73741cc25c0e3d790507657e7350c
bhav2905 <bhavmangal73@gmail.com>
2026-02-09 16:22:42 +0530

    [bhavI.add mplement word count feature in fileoperations.js
    Added functionality to read a file, count words, and write the count to a new file.

--- Q1_File_Operations/fileoperations.js
+++ Q1_File_Operations/fileoperations.js
@@ -1,7 +1,6 @@
 /*Create a Node.js program that reads a text file, counts the number of words, and writes the
  count to a new file.*/
  const fs = require('fs');
-
  // Content to write
  const content = 'This is sample content for our file.';
  
@@ -16,7 +15,7 @@
  
      // Read the file AFTER writing
      const data = fs.readFileSync('input.txt', 'utf8');
- 
+
      // Count words
      const wordCount = data.trim().split(/\s+/).length;
  
